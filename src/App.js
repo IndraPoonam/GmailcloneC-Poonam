66ac9b3972631f2434e956ada5721e12cf01975d
IndraPoonam <iamindra.poonam@gmail.com>
2024-04-03 23:20:39 +0530

    Gmail-clone with some functionlity

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState,useEffect, useContext, createContext, useMemo} from 'react';
+import GmailHeader from './GmailHeader';
+import LeftSideBar from './LeftSideBar';
+import GmailBody from './GmailBody';
+import RightSideBar from './RightSideBar';
+import { Routes,Route } from 'react-router-dom';
 
 function App() {
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<>
+
+
+{/* <Navbar/> */}
+
+    <div class="body-wrapper" >
+    <GmailHeader />
+    <LeftSideBar />
+    
+    <Routes>
+      <Route path="/" element={<GmailBody params = ""/>} />
+      <Route path="/inbox" element={<GmailBody params="inbox"/> } />
+      <Route path="/sent" element={<GmailBody params="sent" />} />
+      <Route path="/draft" element={<GmailBody params="draft" />} />
+    </Routes>
+
+  <RightSideBar />
+
+  </div>
+</>
   );
 }
 
 export default App;
+
